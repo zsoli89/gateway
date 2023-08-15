8e92ce10a0953792e08091de2382d8df138190d5
zsoli89 <jakabzsoli89@gmail.com>
2023-08-15 15:07:11 +0200

    git init gateway

---
+++ src/test/java/hu/webuni/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webuni.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
